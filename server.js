e6b72cfdffb71d977b74efcda8715cb4f4873ffd
Golam Rabby <golamrabby821@gmail.com>
2025-04-04 21:37:18 +0600

    "update"

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
+const passport = require('passport')
+const session = require('express-session')
+
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
@@ -11,8 +14,23 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
+console.log(process.env.SESSION_SECRET)
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false}
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
+
+app.route('/').get((req, res) => {
+  res.render('index', { title: 'Hello', message: 'Please log in'})
 });
 
 const PORT = process.env.PORT || 3000;
