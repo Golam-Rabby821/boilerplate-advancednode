4c49e0e48a4ce98453acd4612713cc26b434056c
Golam Rabby <golamrabby821@gmail.com>
2025-04-05 14:30:09 +0600

    "modularized auth and routes"

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const express = require("express");
 const myDB = require("./connection");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
+const routes = require("./routes.js");
+const auth = require("./auth.js");
+
 const passport = require("passport");
 const session = require("express-session");
 
 const { ObjectID } = require("mongodb");
 
-const LocalStrategy = require("passport-local");
+
+const bcrypt = require("bcrypt");
 
 const app = express();
 
@@ -17,8 +21,6 @@ fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-console.log(process.env.SESSION_SECRET);
 
 app.use(
   session({
@@ -38,69 +40,9 @@ app.set("views", "./views/pug");
 myDB(async (client) => {
   const myDataBase = await client.db("database").collection("users");
 
-  function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return next();
-    }
-    res.redirect("/");
-  }
+  routes(app, myDataBase);
 
-  // Be sure to change the title
-  app.route("/").get((req, res) => {
-    // Change the response to render the Pug template
-    res.render("index", {
-      title: "Connected to Database",
-      message: "Please login",
-      showLogin: true,
-    });
-  });
-
-  app
-    .route("/login")
-    .post(
-      passport.authenticate("local", { failureRedirect: "/" }),
-      (req, res) => {
-        res.redirect("/profile");
-      }
-    );
-
-  app.route("/profile").get(ensureAuthenticated, (req, res) => {
-    res.render("profile", { username: req.user.username});
-  });
-
-  app.route('/logout').get((req, res) => {
-    req.logout();
-    res.redirect('/');
-  })
-
-  app.use((req, res, next) => {
-    res.status(404)
-      .type('text')
-      .send('Not Found');
-  });
-
-  passport.use(
-    new LocalStrategy((username, password, done) => {
-      myDataBase.findOne({ username: username }, (err, user) => {
-        console.log(`User ${username} attempted to log in.`);
-        if (err) return done(err);
-        if (!user) return done(null, false);
-        if (password !== user.password) return done(null, false);
-        return done(null, user);
-      });
-    })
-  );
-  // Serialization and deserialization here...
-  passport.serializeUser((user, done) => {
-    console.log(user._id);
-    done(null, user._id);
-  });
-
-  passport.deserializeUser((id, done) => {
-    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      done(null, doc);
-    });
-  });
+  auth(app, myDataBase);
 
   // Be sure to add this...
 }).catch((e) => {
